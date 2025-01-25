b148ebf261a3c88001a1a0d3838f09ea74e2d15b
camilocbarrera <cristian.correa.cs@gmail.com>
2025-01-25 18:35:01 -0500

    Committing changes for next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['lablab.ai'],
+  },
   // Add other configuration options here
 }
 
