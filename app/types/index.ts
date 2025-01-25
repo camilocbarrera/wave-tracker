7cbfb8e6222bf2db3aa6cada4afc0288916294bc
camilocbarrera <cristian.correa.cs@gmail.com>
2025-01-25 17:23:33 -0500

    Committing changes for app/types/index.ts

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,8 +1,14 @@
 export interface TowerData {
   latitude: string
   longitude: string
+  mcc: string
+  mnc: string
+  lac: string
+  cellId: string
   range: string
+  samples: string
   signalStrength: string
+  radio: string
 }
 
 export interface AnalysisResult {
